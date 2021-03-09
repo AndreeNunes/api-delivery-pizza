7450112c4b7069669914bb46778d9d67c02e1ad8
André Nicola Nunes <57879149+AndreeNunes@users.noreply.github.com>
2021-03-09 18:10:16 -0300

    Commit initial

---
+++ src/main/java/api/delivery/pizza/apideliverypizza/ApiDeliveryPizzaApplication.java
@@ -0,0 +1,13 @@
+package api.delivery.pizza.apideliverypizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeliveryPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeliveryPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/delivery/pizza/apideliverypizza/ApiDeliveryPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package api.delivery.pizza.apideliverypizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeliveryPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
